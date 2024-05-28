57ffba34b4859a3799db49187960419941d6f606
GpapPeaky <giorgospapamat@gmail.com>
2024-05-28 21:20:24 +0300

    Files and folders refactor

--- editor.mk
+++ editor.mk
@@ -1,7 +1,8 @@
 all: editor.o
-	g++ -Isrc/include -Lsrc/lib editor.cpp editor.o -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -o editor
+	g++ -Isrc/include -Lsrc/lib src/editor.cpp obj/editor.o -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -o editor
 
 # RESOURCES FOR COSMETICS, MOSTLY...
 
-editor.o: editor.rc
-	windres -o editor.o editor.rc
+editor.o: resources/editor.rc
+	windres -o obj/editor.o resources/editor.rc
+
--- main.mk
+++ main.mk
@@ -1,7 +1,7 @@
 all: resource.o
-	g++ -Isrc/include -Lsrc/lib main.cpp init_win.cpp weapon.cpp player.cpp mesh.cpp macros.cpp enemy.cpp fx.cpp map_load.cpp item.cpp sound.cpp font.cpp ui.cpp utils.cpp resource.o -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -o Spellixell
+	g++ -Isrc/include -Lsrc/lib src/main.cpp src/init_win.cpp src/weapon.cpp src/player.cpp src/mesh.cpp src/macros.cpp src/enemy.cpp src/fx.cpp src/map_load.cpp src/item.cpp src/sound.cpp src/font.cpp src/ui.cpp src/utils.cpp obj/resource.o -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -o Spellixell
 
 # RESOURCES FOR COSMETICS, MOSTLY...
 
-resource.o: resource.rc
-	windres -o resource.o resource.rc
+resource.o: resources/resource.rc
+	windres -o obj/resource.o resources/resource.rc
